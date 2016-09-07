28a8235263a60b2f841d20e1ebb8f8fad4a3805b
FrankRodelli <frankrodelli93@gmail.com>
2016-09-06 21:08:28 -0400

    Initial full commit

---
+++ Assets/CueBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CueBall : MonoBehaviour {
+
+	Vector3 initPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		initPosition = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Resets ball to initial location of it goes into a pocket 
+		var ballPositionX = transform.position.x;
+		var ballPositionZ = transform.position.z;
+		if (ballPositionX > 3.80 || ballPositionX < -3.80) {
+			transform.position = initPosition;
+			print ("You suck ass dude");
+		
+		}
+	}
+}
---
+++ Assets/StandardBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandardBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Finds location of ball and deletes gameobject if out of range
+		if (transform.position.x > 3.80 || transform.position.x < -3.80) {
+
+			Destroy (gameObject);
+		}
+	
+	}
+}
