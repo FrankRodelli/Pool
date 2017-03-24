2a1ba6f83809a7e4cbfc3eff8fb0d8c2d7ca4800
FrankRodelli <frankrodelli93@gmail.com>
2017-03-24 00:48:00 -0400

    Added trajectory sprite
    Added trajectory sprite and added spinning mechanic around cue ball

---
+++ Assets/Trajectory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Sets change in mouses x axis value to xAxis variable
+        float xAxis = Input.GetAxis("Mouse X");
+
+        //Sets cueball to variable
+        GameObject cue = GameObject.Find("cueball");
+
+        //Gets vector3 of cueball game object
+        Vector3 objectVector3 = cue.transform.position;
+
+        //Sets change in translation if change was less than 0 
+        if(xAxis > 0)
+        {
+            transform.RotateAround(objectVector3, Vector3.up, 10);
+        }
+        
+        //Sets change in translation if change was more than 0
+        if(xAxis <0)
+        {
+            transform.RotateAround(objectVector3, Vector3.down, 10);
+        }
+
+        
+	}
+}
